3791f6d90febfa3ffb098b2edcd5786f271f89b2
com.Kondratov <com.Kondratov@gmail.com>
2020-07-21 11:18:06 +0300

    Добавьте файлы проекта.

---
+++ Punish/Helper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Punish
+{
+   public static class Helper
+    {
+       public static void Chek(this string [] words)
+        {
+            var rnd = new Random();
+            string Word = words[rnd.Next(1, 5)];
+            char[] Letters;
+            Letters = Word.ToCharArray();
+            Console.WriteLine("Вводите слово по буквам, с маленькой буквы");
+            Console.WriteLine($"Первая Буква слова ({Letters[0]}), начните вводить слово с этой буквы");
+            int LetTr = 0 ;
+           
+            for (int i = 0; i < Letters.Length; i++)
+            {
+                char Symbol = Convert.ToChar(Console.ReadLine());
+                if (Letters[i] == Symbol)
+                {
+                    Console.WriteLine("Вы ввели парвильную букву");
+                    LetTr += 1;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели не парвильную букву");
+                }
+            }
+            if (LetTr == Letters.Length)
+            {
+                Console.WriteLine($"Загаданное слово было {Word} , и угадали все слово");
+            }
+            else
+            {
+                Console.WriteLine($"Загаданное слово было {Word} , вы угадали {LetTr} букв");
+            }
+            
+            
+        }
+    }
+}
---
+++ Punish/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Punish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Animals = { "заяц", "волк", "медведь", "енот", "выдра" };
+            string[] Poddbs = { "синтия", "родинка", "бульдозер", "унитаз", "кобра" };
+            string[] Cars = { "мерседес", "вольва", "тесла", "лада", "форд" };
+            Console.WriteLine("Выбирите категорию?");
+            Console.WriteLine("1  - Лесные животные\n2 - Клички в Поддубном\n3 - Название машин ");
+            string Category = (Console.ReadLine());
+            switch (Category)
+            {
+                case "1":
+                    Animals.Chek();
+                    break;
+                case "2":
+                    Poddbs.Chek();
+                    break;
+                case "3":
+                    Cars.Chek();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
+    
